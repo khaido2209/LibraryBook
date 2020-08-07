2572048f6e3c44fe88b027b005b60b7478383928
khaido2209 <58859462+khaido2209@users.noreply.github.com>
2020-08-07 14:50:53 +0700

    Add files via upload

--- library/views.py
+++ library/views.py
@@ -53,3 +53,39 @@ from django.views import generic
 class BookDetailView(generic.DetailView):
     """Generic class-based detail view for a book."""
     model = Book
+    template_name = "library/book_detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(BookDetailView,     self).get_context_data(*args, **kwargs)
+        context['books'] = Book.objects.all()
+
+        return context
+
+# def book_detail_view(request, book_id, cate):
+#     bookitem = Book.objects.all()
+#     book_view = Book.objects.get(pk=book_id)
+#     cate_view= Category.objects.get(pk = category_id)
+#     cateitem = Category.objects.all()
+
+#     context ={
+#         'bookview': book_view,
+#         'book' : bookitem,
+#         'category' : cateitem,
+#         'paging' : bookpage,
+#         'cateview':cate_view,
+#     }
+#     return render(request, 'library/book_detail.html', context)
+    
+# def book_view(request, book_id, category_id):
+#     book_view= Book.objects.get(pk = book_id)
+#     bookitem3 = Book.objects.all()
+#     cate_view= Category.objects.get(pk = category_id)
+#     cateitem = Category.objects.all()
+
+#     context2={
+#         'book' : bookitem,
+#         'bookview': book_view,
+#         'cateview':cate_view,
+#         'catename': cateitem,
+#     }
+#     return render(request, "library/bookview.html", context2)
